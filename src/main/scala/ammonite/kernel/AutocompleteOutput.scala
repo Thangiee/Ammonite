6c9196e1ac13fb390d0340e48602b01840b8ed92
Harshad Deo <subterranean.hominid@gmail.com>
2016-10-05 12:08:22 +0530

    revert to scala-idioms

--- src/main/scala/ammonite/kernel/AutocompleteOutput.scala
+++ src/main/scala/ammonite/kernel/AutocompleteOutput.scala
@@ -1,9 +1,3 @@
 package ammonite.kernel
 
-final class AutocompleteOutput(val names: Seq[String], val signatures: Seq[String])
-
-object AutocompleteOutput{
-
-  def unapply(op: AutocompleteOutput): Option[(Seq[String], Seq[String])] = Some((op.names, op.signatures))
-
-}
+final case class AutocompleteOutput(names: Seq[String], signatures: Seq[String])
--- src/main/scala/ammonite/kernel/LogMessage.scala
+++ src/main/scala/ammonite/kernel/LogMessage.scala
@@ -3,21 +3,7 @@ package ammonite.kernel
 import java.io.{StringWriter, PrintWriter}
 
 sealed trait LogMessage extends Serializable {
-  
   val msg: String
-
-  def accept[D, R](visitor: LogMessageVisitor[D, R], data: D): R
-
-}
-
-trait LogMessageVisitor[D, R]{
-
-  def visitInfo(data: D, msg: String): R
-
-  def visitWarning(data: D, msg: String): R
-
-  def visitError(data: D, msg: String): R
-
 }
 
 private[kernel] object LogMessage {
@@ -32,71 +18,6 @@ private[kernel] object LogMessage {
   }
 }
 
-final class LogError (override val msg: String) extends LogMessage{
-
-  override def accept[D, R](visitor: LogMessageVisitor[D, R], data: D): R = visitor.visitError(data, msg)
-
-  override def hashCode: Int = msg.hashCode
-
-  override def toString: String = s"LogError($msg)"
-
-  override def equals(other: Any): Boolean = other match {
-    case LogError(msg2) => msg2 == msg
-    case _ => false
-  }
-
-}
-
-object LogError{
-
-  def apply(msg: String): LogError = new LogError(msg)
-  
-  def unapply(err: LogError): Option[String] = Some(err.msg)
-
-}
-
-final class LogWarning(override val msg: String) extends LogMessage{
-
-  override def accept[D, R](visitor: LogMessageVisitor[D, R], data: D): R = visitor.visitWarning(data, msg)
-
-  override def hashCode: Int = msg.hashCode
-
-  override def toString = s"LogWarning($msg)"
-
-  override def equals(other: Any): Boolean = other match {
-    case LogWarning(msg2) => msg == msg2
-    case _ => false
-  }
-
-}
-
-object LogWarning{
-
-  def apply(msg: String): LogWarning = new LogWarning(msg)
-
-  def unapply(warn: LogWarning): Option[String] = Some(warn.msg)
-
-}
-
-final class LogInfo(override val msg: String) extends LogMessage{
-
-  override def accept[D, R](visitor: LogMessageVisitor[D, R], data: D): R = visitor.visitInfo(data, msg)
-
-  override def hashCode: Int = msg.hashCode
-
-  override def toString: String = s"LogInfo($msg)"
-
-  override def equals(other: Any): Boolean = other match {
-    case LogInfo(msg2) => msg == msg2
-    case _ => false
-  }
-
-}
-
-object LogInfo{
-
-  def apply(msg: String): LogInfo = new LogInfo(msg)
-
-  def unapply(inf: LogInfo): Option[String] = Some(inf.msg)
-
-}
+final case class LogError (override val msg: String) extends LogMessage
+final case class LogWarning(override val msg: String) extends LogMessage
+final case class LogInfo(override val msg: String) extends LogMessage
